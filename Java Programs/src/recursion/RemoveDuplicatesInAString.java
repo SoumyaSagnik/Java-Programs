90d4fab8221e6152d5f557945fa670fc27365ad7
SoumyaSagnik <soumyasagnik@gmail.com>
2022-11-14 19:53:30 +0530

    Improvement on previous soln.

--- Java Programs/src/recursion/RemoveDuplicatesInAString.java
+++ Java Programs/src/recursion/RemoveDuplicatesInAString.java
@@ -9,6 +9,8 @@ public class RemoveDuplicatesInAString {
 		String s = sc.nextLine();
 		sc.close();
 		System.out.println(removeDuplicates(s, "", 0));
+		System.out.println("----------------------------------");
+		System.out.println(removeDuplicates(s, ""));
 	}
 
 	public static String removeDuplicates(String s, String newString, int index) {
@@ -18,5 +20,18 @@ public class RemoveDuplicatesInAString {
 			return removeDuplicates(s, newString, index + 1);
 		else
 			return removeDuplicates(s, newString + String.valueOf(s.charAt(index)), index + 1);
+	}
+
+	public static String removeDuplicates(String s, String newString) {
+		if (s.length() == 1) {
+			if (newString.contains(s))
+				return newString;
+			else
+				return newString + s;
+		}
+		if (newString.contains(s.substring(0, 1))) {
+			return removeDuplicates(s.substring(1), newString);
+		} else
+			return removeDuplicates(s.substring(1), newString + s.charAt(0));
 	}
 }
