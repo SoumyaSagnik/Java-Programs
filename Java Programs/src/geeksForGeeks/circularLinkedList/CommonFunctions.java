f7f63a227bc771f924b7e05d850c836b56cdf8da
SoumyaSagnik <soumyasagnik@gmail.com>
2022-12-14 02:53:16 +0530

    CLL CF

--- Java Programs/src/geeksForGeeks/circularLinkedList/CommonFunctions.java
+++ Java Programs/src/geeksForGeeks/circularLinkedList/CommonFunctions.java
@@ -2,7 +2,7 @@ package geeksForGeeks.circularLinkedList;
 
 public class CommonFunctions {
 	public void printList(Node head) {
-		if(head == null)
+		if (head == null)
 			return;
 		Node curr = head;
 		while (curr.next != head) {
@@ -11,5 +11,23 @@ public class CommonFunctions {
 		}
 		System.out.print(curr.data + " ");
 		System.out.println();
+	}
+
+	public Node createNodes(int[] arr) {
+		if (arr == null || arr.length == 0)
+			return null;
+		Node head = new Node(arr[0]);
+		if (arr.length == 1) {
+			head.next = head;
+			return head;
+		}
+		Node curr = head;
+		for (int i = 1; i < arr.length; i++) {
+			Node n = new Node(arr[i]);
+			curr.next = n;
+			curr = n;
+		}
+		curr.next = head;
+		return head;
 	}
 }
